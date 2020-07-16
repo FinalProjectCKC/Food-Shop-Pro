6c078175c938cdd57c4e834fe60f290046b4d8bf
luongdinhkhanhvinh <luongdkvinh@gmail.com>
2020-07-16 13:33:05 +0700

    add image in project

---
+++ src/components/avatar/Avatar.js
@@ -0,0 +1,19 @@
+import React from "react";
+import PropTypes from "prop-types";
+
+const avatarsSizes = {
+  small: 34,
+  medium: 50,
+  large: 75,
+  xlarge: 150,
+};
+const defaultEditButton = {
+  name: "mode-edit",
+  type: "material",
+  color: "#fff",
+  underlayColor: "#000",
+};
+
+const Avatar = () => {};
+
+export default Avatar;
